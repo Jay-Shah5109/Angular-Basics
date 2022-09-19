c733d4b15c63a6e6ad6893c53f7c5695d49ca634
jay <jayshah5109@gmail.com>
2022-09-19 20:02:59 +0530

    basics added until pipes tutorial

--- my-app/src/app/test/test.component.ts
+++ my-app/src/app/test/test.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
-  selector: '[app-test]',
+  selector: 'app-test',
   template: `<div>
   Welcome {{name}}
   </div>
   <h2>{{2+2}}</h2>
   <h2  [class]=successClass>{{name.length}}</h2>
-  
-  <h2>{{name.toUpperCase()}}</h2>
-  
+
+  <h2>{{name | slice:1:9 | uppercase}}</h2>
+
   <h2 [class]=successClass class="text-danger" >{{greetUser()}}</h2>
 
-  <h2 [class.text-special]="hasError">Jay Rohit Shah</h2>
+  <h2 [class.text-special]="hasError">Jay Shah</h2>
 
   <input [id]="id" type="text" value="Jay">
   // <input [disabled]="isDisabled" [id]="id" type="text" value="Jay">
@@ -24,6 +25,49 @@ import { Component, OnInit } from '@angular/core';
   <h2 [style.color]="highlightColor">Style Binding 2</h2>
 
   <h2 [ngStyle]="titleStyles">Style Binding 2</h2>
+  <br>
+ <button (click)="onClick($event)">Greet</button>
+ {{greeting}}
+
+ <input #myInput type="text">
+ <button (click)="logMessage(myInput.value)">Log</button>
+
+ <input [(ngModel)]="name" type="text">
+ {{name}}
+
+ <h2 *ngIf="displayName; else elseBlock">Using ngIf directive</h2>
+
+<ng-template #elseBlock>
+ <h2>Name is hidden, since variable is set to false</h2>
+ </ng-template>
+
+  <div *ngIf="displayName; then thenBlock; else elseBlock1"></div>
+
+ <ng-template #thenBlock>
+  <h2>Then block</h2>
+  </ng-template>
+
+  <ng-template #elseBlock1>
+   <h2>Hidden block</h2>
+   </ng-template>
+
+
+   <div [ngSwitch]="color">
+   <div *ngSwitchCase="'red'">you have picked red color</div>
+   <div *ngSwitchCase="'green'">you have picked green color</div>
+   <div *ngSwitchCase="'brown'">you have picked brown color</div>
+   <div *ngSwitchDefault>No color available</div>
+   </div>
+
+   <div *ngFor="let color of colors, first as i">
+   <h3>{{i}}-{{color}}</h3>
+   </div>
+
+   <h2>Using component interaction here, printing {{myvar}}</h2>
+
+   <button (click)="fireEvent()">Press here for child to parent interaction</button>
+
+   <h4>{{Person | json}}</h4>
   `,
   styles:[`
     .text-success {
@@ -39,7 +83,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  public name = "Jay Shah";
+  public name = "temporary text";
   public id = "abc123";
   public isDisabled = true;
   public siteURL = window.location.href;
@@ -51,14 +95,36 @@ export class TestComponent implements OnInit {
     "text-special": this.isSpecial
   }
 
+  public greeting = "";
+  public message = "";
+  public displayName=true;
+
   public highlightColor = "orange";
+
+  public color="sss";
+
+  @Input("parentData") public myvar:any;
+
+  public colors=["red","blue","black","brown"];
 
   public titleStyles = {
     color: "lightblue",
     fontStyle: "italic"
   }
 
+  public Person = {
+    "name":"Jay",
+    "lastName":"Shah"
+  }
+
   public successClass = "text-success";
+
+  @Output() public childEvent = new EventEmitter();
+
+  fireEvent() {
+    this.childEvent.emit("Hey, this is data from child component..");
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -67,6 +133,15 @@ export class TestComponent implements OnInit {
   // function definition
   greetUser() {
     return "Returning from greetUser function:  "+this.name+" and site URL is: "+this.siteURL;
+  }
+
+  onClick(event:any) {
+    console.log(event);
+    this.greeting = event.type;
+  }
+
+  logMessage(value:any) {
+    console.log(value);
   }
 
 }
