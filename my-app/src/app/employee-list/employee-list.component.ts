350d9b6207d31644097d85eaf7fe6a7291fa10d8
jay <jayshah5109@gmail.com>
2022-09-21 12:06:35 +0530

    handling errors in HTTP

--- my-app/src/app/employee-list/employee-list.component.ts
+++ my-app/src/app/employee-list/employee-list.component.ts
@@ -5,6 +5,7 @@ import { EmployeeService } from '../employee.service';
   selector: 'employee-list',
   template: `
   <h2>Employee List</h2>
+  <h6>{{errorMsg}}</h6>
   <ul *ngFor="let employee of employees">
     <li>{{employee.name}}</li>
   </ul>
@@ -16,9 +17,11 @@ export class EmployeeListComponent implements OnInit {
   constructor(private _employeeService: EmployeeService) { }
 
   public employees = [] as any;
+  public errorMsg;
 
   ngOnInit(): void {
-  this._employeeService.getEmployees().subscribe(data => this.employees=data);
+  this._employeeService.getEmployees().subscribe(data => this.employees=data,
+                    error => this.errorMsg = error);
   }
 
 }
