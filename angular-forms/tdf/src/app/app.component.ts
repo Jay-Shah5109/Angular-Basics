fa46d38e6340241fe41bd2ddeceb3752447a0ca3
jay <jayshah5109@gmail.com>
2022-09-26 00:09:35 +0530

    added submission feature for form data

--- angular-forms/tdf/src/app/app.component.ts
+++ angular-forms/tdf/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,23 @@ export class AppComponent {
   topicHasError=true;
   userModel=new User('','jay@test.com',4567812345,'default','morning',true);
 
+  constructor(private _enrollmentservice: EnrollmentService) {
+
+  }
+
   validateTopic(value: any) {
     if(value === 'default') {
       this.topicHasError=true;
     } else {
       this.topicHasError=false;
     }
+  }
+
+  onSubmit() {
+//     console.log(this.userModel);
+  this._enrollmentservice.enroll(this.userModel).subscribe(
+    data => console.log('Success!', data);
+    error => console.log('Error!', error);
+    )
   }
 }
